7075e0db5924ddf783cba0ed4b2b4d3993cff062
Laura Nuñez <njm0031533@est.univalle.edu>
2024-12-04 22:46:19 -0400

    Alert/:id agregado

--- server.js
+++ server.js
@@ -294,6 +294,33 @@ app.put('/drones/:droneId', async (req, res) => {
   }
 });
 
+app.get('/alert/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const query = `
+      SELECT 
+        alerttype, latitude, longitude, status, userid, 
+        description, droneid, address, images 
+      FROM alert 
+      WHERE id = $1;
+    `;
+
+    const values = [id];
+    const result = await client.query(query, values);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Alert not found' });
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error('Error fetching alert:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 //Movil
 app.post('/alert', async (req, res) => {
   const {
